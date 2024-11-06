d9a86f24e8e0db711c1a891862a7e3f05ee2164a
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-11-06 23:57:36 +0530

    Create BodyPart

---
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "UI/UIElement/ImageView.h"
+#include "Direction.h"
+
+
+namespace Player
+{
+	class BodyPart
+	{
+	protected:
+		UI::UIElement::ImageView* bodypart_image;
+
+		sf::Vector2i grid_position;
+		Direction direction;
+
+		float bodypart_width;
+		float bodypart_height;
+	}
+
+}
